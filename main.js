6657680ed54a8da970678e413c8e46167b0316f4
mollywhitnack <mollywhitnack@gmail.com>
2016-06-12 15:06:14 -0700

    weather for sols and days

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 'use strict'
 
-var weatherURL = `http://marsweather.ingenology.com/v1/archive/?format=jsonp`;
+var weatherURL ='' ;//`http://marsweather.ingenology.com/v1/archive/?format=jsonp`;
 var found  =false;
 var dayforW =''
+var dayBool = false;
+var solBool = false;
+var sol = 0;
 
 $(document).ready(init); 
 function init(){
@@ -13,40 +16,34 @@ function init(){
 }
 
 function dayImages(){
+  weatherURL = `http://marsweather.ingenology.com/v1/archive/?format=jsonp`;
+  dayBool = true;
+  solBool = false;
+  found = false;
   $('.tempImage').empty();
   $('.tempTarget').empty();
-  weather();
   var day = $('.dayInput').val();
   dayforW = day;
+  weather();
   var nDate  = [];
-  //console.log("day:", day);
   var date = day.split('-');
-  //console.log("date:", date);
   for(let i=0; i<date.length; i++){
     var digit = date[i].split('');
-    //console.log("digit: ", digit.length);
     if(digit.length == 2){
-      //console.log("at 0: ", digit[0]);
       digit = digit.splice(1, 1);
-      //console.log(digit);
     }
     digit = digit.join('');
     nDate.push(digit);
   }
   nDate = nDate.join('-');
-  //console.log(nDate);
   $.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${nDate}&api_key=MinS57PfHb5uC8drEsRM4zEk3SoQSq57p4N7juwu`)
   .done(function(data){
-    //console.log("data: " ,data);
     var length = data.photos.length;
-    //console.log("length:", length);
 
     for(let i =0; i<length; i++){
       var url = (data.photos[i].img_src).toString();
-      //console.log(url);
       var $li2 = $('.templateTarget').clone();
       $li2.attr("data-slide-to", `${i}`);
-      //console.log("myCarousel:", $li2.find('.caro'));
       if(i === 0){
         console.log("i=0");
         console.log($li2);
@@ -62,28 +59,33 @@ function dayImages(){
     }
    })
    .fail(function(){
-    console.log('Error!');
-    alert("No images available, please refine your search!");
+    console.log('earth day Error!');
+    //alert("No images available, please refine your search!");
    });
 }
 
 
 function solImages(){
+  weatherURL = `http://marsweather.ingenology.com/v1/archive/?format=jsonp`;
+  dayBool = false;
+  solBool = true;
+  found = false;
   $('.tempImage').empty();
   $('.tempTarget').empty();
-  var sol = $('.solInput').val();
+  sol = $('.solInput').val();
+  weather();
   var camera = $('.cameraList').val();
   $.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&camera=${camera}&api_key=MinS57PfHb5uC8drEsRM4zEk3SoQSq57p4N7juwu`)
   .done(function(data){
-    console.log("data: " ,data);
+    //console.log("data: " ,data);
     var length = data.photos.length;
     console.log("length:", length);
     for(let i =0; i<length; i++){
       var url = (data.photos[i].img_src).toString();
-      console.log(url);
+      //console.log(url);
       var $li2 = $('.templateTarget').clone();
       $li2.attr("data-slide-to", `${i}`);
-      console.log("myCarousel:", $li2.find('.caro'));
+      //console.log("myCarousel:", $li2.find('.caro'));
       if(i == 0){
         $li2.addClass("active");
         $('.tempImage').append(`<div class="item active"> <img src = "${url}"/> </div>`);
@@ -95,83 +97,87 @@ function solImages(){
     }
    })
    .fail(function(){
-    console.log('Error!');
+    console.log('Mars sol Error!');
     alert("No images available, please refine your search!");
    });
 }
 
 function weather(){
-  console.log("weather");
-  $.ajax({
-  url: weatherURL,
-  method: 'GET', //defualt  is get, dont really need this
-  dataType: 'jsonp',
-  success: function(data){
-    for(var i = 0; i<data.results.length; i++){
-      console.log(data.results[i].terrestrial_date);
-      if(data.results[i].terrestrial_date === dayforW){
-        var weatherdata = data.results[i].atmo_opacity;
-        var minFar = data.results[i].min_temp_fahrenheit;
-        console.log(minFar);
-        var maxFar = data.results[i].max_temp_fahrenheit;
-        console.log(maxFar);
-        found = true;
-        console.log("Found: ", weatherdata);
-        $('.weatherReport').text('');
-        $('.weatherReport').show();
+  console.log("solBool: " , solBool);
+  console.log("daylBool: " , dayBool);
+  if(dayBool == true){
+    $('.weatherReport').text(`...Loading Weather for ${dayforW}`);
+    $('.weatherReport').show();
 
-        $('.weatherReport').empty().text(`${weatherdata},  High: ${maxFar}F, Low: ${maxFar}F`);
-      }
+    //console.log("weather");
+    $.ajax({
+    url: weatherURL,
+    method: 'GET', //defualt  is get, dont really need this
+    dataType: 'jsonp',
+    success: function(data){
+      for(var i = 0; i<data.results.length; i++){
+          //console.log(data.results[i].terrestrial_date);
+          if(data.results[i].terrestrial_date === dayforW){
+            var weatherdata = data.results[i].atmo_opacity;
+            var minFar = data.results[i].min_temp_fahrenheit;
+            //console.log(minFar);
+            var maxFar = data.results[i].max_temp_fahrenheit;
+            //console.log(maxFar);
+            found = true;
+            //console.log("Found: ", weatherdata);
+            $('.weatherReport').text('');
+            var $h3 = $('<h3>');
+            $('.weatherReport').text(`${weatherdata} -- High: ${maxFar} F Low: ${minFar} F`);
+          }
+        }
+        if(found === false){
+          var next = data.next;
+          weatherURL = next;
+          //console.log("next: " , next);
+          weather();
+        }
+    },
+    error: function(error){
+      console.log("Weather Error");
     }
-    if(found === false){
-      var next = data.next;
-      weatherURL = next;
-      console.log("next: " , next);
-      weather();
-      //weather
-    }
-
-  },
-  error: function(error){
-    console.log("Weather Error");
-    renderError(error);
+    });
   }
 
-
-});
-}
-
-/*function cameraImages(){
-  $('.tempImage').empty();
-  $('.tempTarget').empty();
-  var camera = $('.cameraList').val();
-  console.log("day:", camera);
-  /*$.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${day}&api_key=MinS57PfHb5uC8drEsRM4zEk3SoQSq57p4N7juwu`)
-  .done(function(data){
-    console.log("data: " ,data);
-    var length = data.photos.length;
-    console.log("length:", length);
-
-    for(let i =0; i<length; i++){
-      var url = (data.photos[i].img_src).toString();
-      console.log(url);
-      var $li2 = $('.templateTarget').clone();
-      $li2.attr("data-slide-to", `${i}`);
-      console.log("myCarousel:", $li2.find('.caro'));
-      if(i == 0){
-        $li2.addClass("active");
-        $('.tempImage').append(`<div class="item active"> <img src = "${url}"/> </div>`);
-      }
-      else
-        $('.tempImage').append(`<div class="item"> <img src = "${url}"/></div>`);
-      $li2.removeClass('templateTarget');
-
-      $('.tempTarget').append($li2);
-      $li2.removeClass("active");
+  else{
+    $('.weatherReport').text(`...Loading Weather for sol ${sol}`);
+    $('.weatherReport').show();
+    //console.log("weather");
+    $.ajax({
+    url: weatherURL,
+    method: 'GET', //defualt  is get, dont really need this
+    dataType: 'jsonp',
+    success: function(data){
+      for(var i = 0; i<data.results.length; i++){
+          //console.log(data.results[i].sol);
+          if(data.results[i].sol.toString() === sol){
+            var weatherdata = data.results[i].atmo_opacity;
+            var minFar = data.results[i].min_temp_fahrenheit;
+            //console.log(minFar);
+            var maxFar = data.results[i].max_temp_fahrenheit;
+            //console.log(maxFar);
+            found = true;
+            //console.log("Found: ", weatherdata);
+            $('.weatherReport').text('');
+            var $h3 = $('<h3>');
+            $('.weatherReport').text(`${weatherdata} -- High: ${maxFar} F Low: ${minFar} F`);
+          }
+        }
+        if(found === false){
+          var next = data.next;
+          weatherURL = next;
+          //console.log("next: " , next);
+          weather();
+        }
+    },
+    error: function(error){
+      console.log("Weather Error");
     }
-   })
-   .fail(function(){
-    console.log('Error!');
-   });
-}*/
+    });
+  }
+}
 
